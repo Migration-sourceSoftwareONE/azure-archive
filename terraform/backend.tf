768c3ceab82139b11db1d331f5cecfe493f1f372
michal-sermak <159771741+michal-sermak@users.noreply.github.com>
2025-06-16 11:16:23 +0200

    Delete terraform/backend.tf

--- terraform/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "rg-github-archives"
-    storage_account_name = "ghrepoarchive123"
-    container_name       = "security-backups"
-    key                  = "terraform.tfstate"
-  }
-}
